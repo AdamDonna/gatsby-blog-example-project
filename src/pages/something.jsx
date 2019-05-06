837aa8ebccecbe9f6da13cc1f6b3b5fe936e15f1
Adam Donaghy <13944590+AdamDonna@users.noreply.github.com>
2019-05-06 23:24:48 +1000

    New pages auto deploy test

---
+++ src/pages/something.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+class NewPage extends React.Component {
+    render() {
+        return <h1>Hello</h1>
+    }
+}
+
+
+export default NewPage
