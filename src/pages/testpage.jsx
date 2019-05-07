22474c7f313fee789a8486747b3ca533991b215b
Adam Donaghy <13944590+AdamDonna@users.noreply.github.com>
2019-05-07 12:57:58 +1000

    Stuff

---
+++ src/pages/testpage.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import Layout from "../components/layout"
+
+class NewPage extends React.Component {
+    render() {
+        return <Layout>Hello world</Layout>
+    }
+}
+
+
+export default NewPage
